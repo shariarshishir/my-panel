81ab7622d1baf0fdb2e74ea72bb2064e5680b914
Imran Kader Chowdhury <kaderchowdhury@merchantbay.com>
2022-11-10 15:10:19 +0600

    Non subscription html

--- resources/views/rfq/_matched_supplier_by_rfq.blade.php
+++ resources/views/rfq/_matched_supplier_by_rfq.blade.php
@@ -42,115 +42,143 @@
                     <div class="supplier-matched-selected-box"></div>
                 </div>
             </div>
-            <div class="new_rfq_filter_wrapper">
-                <div class="new_rfq_filter_select">
-                    <div class="input-field">
-                        <label>
-                            <input type="checkbox" id="select-all-supplier" name="select-all-supplier" />
-                            <span>Select All</span>
-                        </label>
+
+            @php
+                $cookie = Cookie::get('sso_token');
+                $cookie = base64_decode(explode(".",$cookie)[1]);
+                $cookie = json_decode(json_decode(json_encode($cookie)));
+            @endphp
+
+            @if($cookie->subscription_status == 1)
+                <div class="new_rfq_filter_wrapper">
+                    <div class="new_rfq_filter_select">
+                        <div class="input-field">
+                            <label>
+                                <input type="checkbox" id="select-all-supplier" name="select-all-supplier" />
+                                <span>Select All</span>
+                            </label>
+                        </div>
+                    </div>
+                    <a class="waves-effect waves-light btn modal-trigger request-for-quotation-modal-trigger" id="request-for-quotation-from-rfq-button" href="#request-for-quotation-from-rfq" >Request for Quotation</a>
+                </div>
+                <!-- Modal Structure -->
+                <div id="request-for-quotation-from-rfq" class="modal">
+                    <div class="modal-content">
+                        <h4>Request for quotation</h4>
+                        <div class="request-for-quotation-from-rfq-profile-count-message">
+                            <p>Are you sure about requesting for quotation to</p>
+                            <p id="request-for-quotation-from-rfq-profile-count"></p>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="javascript:void(0)" class="btn btn_green" onclick='onRequestSubmit()'>Submit</a>
+                        <a href="javascript:void(0)" class="btn btn_green modal-action modal-close waves-effect waves-green btn-flat">Close</a>
                     </div>
                 </div>
-                <a class="waves-effect waves-light btn modal-trigger request-for-quotation-modal-trigger" id="request-for-quotation-from-rfq-button" href="#request-for-quotation-from-rfq" >Request for Quotation</a>
-            </div>
-            <!-- Modal Structure -->
-            <div id="request-for-quotation-from-rfq" class="modal">
-                <div class="modal-content">
-                    <h4>Request for quotation</h4>
-                    <div class="request-for-quotation-from-rfq-profile-count-message">
-                        <p>Are you sure about requesting for quotation to</p>
-                        <p id="request-for-quotation-from-rfq-profile-count"></p>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <a href="javascript:void(0)" class="btn btn_green" onclick='onRequestSubmit()'>Submit</a>
-                    <a href="javascript:void(0)" class="btn btn_green modal-action modal-close waves-effect waves-green btn-flat">Close</a>
-                </div>
-            </div>
 
-            <div class="rfq_new_layout_match_supplier_with_rfq">
-                <div class="match_supplier_rfq_single_wrapper">
-                    <div class="row single_wraper_gapping">
+                <div class="rfq_new_layout_match_supplier_with_rfq">
+                    <div class="match_supplier_rfq_single_wrapper">
+                        <div class="row single_wraper_gapping">
 
-                        @foreach($businessProfiles as $businessProfile)
-                        <div class="col s12 m4 matched_supplier_item">
-                            <!-- new -->
-                            <div class="match_supplier_rfq_single_content">
-                                <div class="input-field">
-                                    <label>
-                                        <input type="checkbox" id="{{$businessProfile['id']}}" user_id="{{$businessProfile['user_id']}}" name="remember" onclick='onProfileSelected(this)'>
-                                        <span></span>
-                                    </label>
-                                </div>
-                                <div class="match_supplier_rfq_single_content_inner_part">
+                            @foreach($businessProfiles as $businessProfile)
+                            <div class="col s12 m4 matched_supplier_item">
+                                <!-- new -->
+                                <div class="match_supplier_rfq_single_content">
+                                    <div class="input-field">
+                                        <label>
+                                            <input type="checkbox" id="{{$businessProfile['id']}}" user_id="{{$businessProfile['user_id']}}" name="remember" onclick='onProfileSelected(this)'>
+                                            <span></span>
+                                        </label>
+                                    </div>
+                                    <div class="match_supplier_rfq_single_content_inner_part">
 
-                                    <!-- First div part -->
-                                    <div class="row sparkle_part">
-                                        <div class="col s12 m3">
-                                            <div class="image_width_wrap">
-                                                <img class="image_width" src='{{Storage::disk('s3')->url('public/'.$businessProfile['business_profile_logo'])}}' alt="">
+                                        <!-- First div part -->
+                                        <div class="row sparkle_part">
+                                            <div class="col s12 m3">
+                                                <div class="image_width_wrap">
+                                                    <img class="image_width" src='{{Storage::disk('s3')->url('public/'.$businessProfile['business_profile_logo'])}}' alt="">
+                                                </div>
+
                                             </div>
-
-                                        </div>
-                                        <div class="col s12 m5">
-                                            <div class="sparkle_knit">
-                                                <h3>{{$businessProfile['business_name']}}</h3>
-                                                <p>{{$businessProfile['location']}}</p>
+                                            <div class="col s12 m5">
+                                                <div class="sparkle_knit">
+                                                    <h3>{{$businessProfile['business_name']}}</h3>
+                                                    <p>{{$businessProfile['location']}}</p>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="col s12 m4">
-                                            <div class="middle_wrap">
-                                                <span class="check_circle">
-                                                    @if($businessProfile['profile_verified_by_admin'] == 1)
-                                                    <i class="material-icons">check_circle</i>
-                                                    @else
-                                                    <i class="material-icons">close_circle</i>
-                                                    @endif
-                                                </span>
-                                                <span class="icon_wrap">
-                                                    @foreach(json_decode($businessProfile['company_overview']['data']) as $data)
-                                                        @if($data->name == 'year_of_establishment')
-                                                            {{date("Y")-$data->value}}+
+                                            <div class="col s12 m4">
+                                                <div class="middle_wrap">
+                                                    <span class="check_circle">
+                                                        @if($businessProfile['profile_verified_by_admin'] == 1)
+                                                        <i class="material-icons">check_circle</i>
+                                                        @else
+                                                        <i class="material-icons">close_circle</i>
                                                         @endif
-                                                    @endforeach
-                                                </span>
+                                                    </span>
+                                                    <span class="icon_wrap">
+                                                        @foreach(json_decode($businessProfile['company_overview']['data']) as $data)
+                                                            @if($data->name == 'year_of_establishment')
+                                                                {{date("Y")-$data->value}}+
+                                                            @endif
+                                                        @endforeach
+                                                    </span>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <!-- Second div part -->
-                                    <div class="middle_part_image_wrapper">
-                                        <h6>Certification:</h6>
-                                        <div class="inner_content_image">
-                                            @foreach($businessProfile['certifications'] as $cert)
-                                                <img class="" src='{{Storage::disk('s3')->url('public/'.$cert['image'])}}' alt="">
-                                            @endforeach
+                                        <!-- Second div part -->
+                                        <div class="middle_part_image_wrapper">
+                                            <h6>Certification:</h6>
+                                            <div class="inner_content_image">
+                                                @foreach($businessProfile['certifications'] as $cert)
+                                                    <img class="" src='{{Storage::disk('s3')->url('public/'.$cert['image'])}}' alt="">
+                                                @endforeach
+                                            </div>
                                         </div>
-                                    </div>
-                                    <!-- Third div part -->
-                                    <div class="main_product_wrap">
-                                        <h6>Main Products:</h6>
-                                        <div class="row main_product_inner">
-                                        <div class="col s12 m10">
-                                            @foreach(json_decode($businessProfile['company_overview']['data']) as $data)
-                                                @if($data->name == 'main_products')
-                                                    <h5>{{$data->value}}</h5>
-                                                @endif
-                                            @endforeach
-                                        </div>
-                                            <div class="col s12 m2 chatbox_wrap">
-                                                <img src="./images/chat-img.png" alt=""> <span>5</span>
+                                        <!-- Third div part -->
+                                        <div class="main_product_wrap">
+                                            <h6>Main Products:</h6>
+                                            <div class="row main_product_inner">
+                                            <div class="col s12 m10">
+                                                @foreach(json_decode($businessProfile['company_overview']['data']) as $data)
+                                                    @if($data->name == 'main_products')
+                                                        <h5>{{$data->value}}</h5>
+                                                    @endif
+                                                @endforeach
+                                            </div>
+                                                <div class="col s12 m2 chatbox_wrap">
+                                                    <img src="./images/chat-img.png" alt=""> <span>5</span>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        @endforeach
+                            @endforeach
 
+                        </div>
+                    </div>
+
+                </div>
+            @else
+                <div class="non-subscribe-message-block">
+                    <div class="non-subscribe-block-text">
+                        <p>We have {{count($businessProfiles)}} suppliers matched</p>
+                        <p>with your Quotation.</p>
+                    </div>
+                    <div class="row">
+                        <div class="col s6 m6">
+                            <p>Please subscribe to see the suppliers</p>
+                            <a href="javascript:void(0);" class="btn btn_green">Subscribe</a>
+                        </div>
+                        <div class="col s6 m6">
+                            <p>Get back to you with in 24 hours</p>
+                            <a href="javascript:void(0);" class="btn btn_green">Submit as Guest</a>
+                        </div>
                     </div>
                 </div>
+            @endif
 
-            </div>
+
         </div>
 @endsection
 @push('js')
