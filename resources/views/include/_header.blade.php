48e0bf92cd6ecb04ea9502921ad2dc3f46830a07
Shariar Shishir <shariar.shishir@merchantbay.com>
2022-11-17 13:47:08 +0600

    Supplier list hide for non subscribe users and oms sso response added

--- resources/views/include/_header.blade.php
+++ resources/views/include/_header.blade.php
@@ -649,7 +649,7 @@
 			</a>
 		</li>
         <li class="oms">
-			<a class="tooltipped" data-position="right" data-tooltip="OMS" href="https://app.merchantbay.com/console/" target="_blank">
+			<a class="tooltipped" data-position="right" data-tooltip="OMS" href="https://app.merchantbay.com/console/?token={{Cookie::get('sso_token_oms')}}" target="_blank">
                 <span>OMS</span>
             </a>
 		</li>
